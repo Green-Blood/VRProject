1a1a315b935e250fb926678521b3c19d59d771eb
JeyOdilkhujaev <j.odilkhujaev@gamesredrift.com>
2022-07-04 18:42:48 +0200

    Second commit

--- Assets/Scripts/MazeDrawer.cs
+++ Assets/Scripts/MazeDrawer.cs
@@ -31,40 +31,57 @@ public sealed class MazeDrawer : MonoBehaviour
             {
                 var cell = maze[i, j];
                 var position = new Vector3(-width / 2 + i, 0, -height / 2 + j);
-                if (cell.HasFlag(WallState.Up))
-                {
-                    var offset = new Vector3(0, 0, size / 2);
-                    var rotation = transform.localRotation.eulerAngles;
-                    InstantiateWall(position, offset, rotation);
-                }
-
-                if (cell.HasFlag(WallState.Left))
-                {
-                    var offset = new Vector3(-size / 2, 0, 0);
-                    var rotation = new Vector3(0, 90, 0);
-                    InstantiateWall(position, offset, rotation, size);
-                }
-
-                if (i == width - 1)
-                {
-                    if (cell.HasFlag(WallState.Right))
-                    {
-                        var offset = new Vector3(+size / 2, 0, 0);
-                        var rotation = new Vector3(0, 90, 0);
-                        InstantiateWall(position, offset, rotation, size);
-                    }
-                }
-
-                if (j == 0)
-                {
-                    if (cell.HasFlag(WallState.Down))
-                    {
-                        var offset = new Vector3(0, 0, -size / 2);
-                        var rotation = transform.localRotation.eulerAngles;
-                        InstantiateWall(position, offset, rotation);
-                    }
-                }
+                CreateUpWall(cell, position);
+                CreateLeftWall(cell, position);
+                CreateRightWall(i, cell, position);
+                CreateDownWall(j, cell, position);
             }
+        }
+    }
+
+    private void CreateDownWall(int j, WallState cell, Vector3 position)
+    {
+        if (j == 0)
+        {
+            if (cell.HasFlag(WallState.Down))
+            {
+                var offset = new Vector3(0, 0, -size / 2);
+                var rotation = transform.localRotation.eulerAngles;
+                InstantiateWall(position, offset, rotation);
+            }
+        }
+    }
+
+    private void CreateRightWall(int i, WallState cell, Vector3 position)
+    {
+        if (i == width - 1)
+        {
+            if (cell.HasFlag(WallState.Right))
+            {
+                var offset = new Vector3(+size / 2, 0, 0);
+                var rotation = new Vector3(0, 90, 0);
+                InstantiateWall(position, offset, rotation, size);
+            }
+        }
+    }
+
+    private void CreateLeftWall(WallState cell, Vector3 position)
+    {
+        if (cell.HasFlag(WallState.Left))
+        {
+            var offset = new Vector3(-size / 2, 0, 0);
+            var rotation = new Vector3(0, 90, 0);
+            InstantiateWall(position, offset, rotation, size);
+        }
+    }
+
+    private void CreateUpWall(WallState cell, Vector3 position)
+    {
+        if (cell.HasFlag(WallState.Up))
+        {
+            var offset = new Vector3(0, 0, size / 2);
+            var rotation = transform.localRotation.eulerAngles;
+            InstantiateWall(position, offset, rotation);
         }
     }
 
